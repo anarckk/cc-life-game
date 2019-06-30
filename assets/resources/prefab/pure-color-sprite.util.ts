e5d018d69ae49abd9d2491200f63673ba55869ed
anarckk <anarckk@gmail.com>
2019-06-30 13:13:44 +0800

    补上未提交的代码

--- assets/resources/prefab/pure-color-sprite.util.ts
+++ assets/resources/prefab/pure-color-sprite.util.ts
@@ -2,9 +2,7 @@
  * Created by kkcra on 2019/1/6
  */
 import { Observable } from 'rxjs';
-import { map }        from 'rxjs/operators';
 import Prefab = cc.Prefab;
-import instantiate = cc.instantiate;
 
 export function loadPureColorSpritePrefab(): Observable<Prefab> {
     return new Observable(observable => {
@@ -13,15 +11,4 @@ export function loadPureColorSpritePrefab(): Observable<Prefab> {
             observable.complete();
         });
     });
-}
-
-export function loadLifeGameBlock(): Observable<cc.Node> {
-    return loadPureColorSpritePrefab().pipe(
-        map(prefab => {
-            const node = new cc.Node();
-            const pNo = prefab.data;
-            const content = instantiate(prefab.data);
-            return node;
-        })
-    );
 }
